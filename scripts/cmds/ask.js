372ab07f7be452797ffe72a21a3a0aea948ddf01
ʬʆʬ Sønïč ʬʆʬ <danielthiam54@gmail.com>
2025-01-01 18:28:50 +0100

    Update ask.js

--- scripts/cmds/ask.js
+++ scripts/cmds/ask.js
@@ -1,34 +1,66 @@
-const axios = require('axios');
-
+const { getPrefix, getStreamFromURL, uploadImgbb } = global.utils;
+async function ai({ message: m, event: e, args: a, usersData: u }) {
+  var p = [`${await getPrefix(e.threadID)}${this.config.name}`,
+`${this.config.name}`
+/*"sexybot"
+*you can add more prefix here
+*/
+]; 
+ if (p.some(b => a[0].toLowerCase().startsWith(b))) {
+try {      
+let prompt = "";
+if (e.type === "message_reply" && e.messageReply.attachments && e.messageReply.attachments[0]?.type === "photo") {
+ const b = await uploadImgbb(e.messageReply.attachments[0].url);
+prompt = a.slice(1).join(" ") + ' ' + b.image.url;
+} else {
+ prompt = a.slice(1).join(" ");
+}
+ var __ = [{ id: e.senderID, tag: await u.getName(e.senderID) }];
+ const r = await require("axios").post(`https://test-ai-ihc6.onrender.com/api`, {
+  prompt: prompt,
+ apikey: "GayKey-oWHmMb1t8ASljhpgSSUI",
+  name: __[0]['tag'],
+ id: __[0]['id'],
+ });
+var _ = r.data.result.replace(/{name}/g, __[0]['tag']).replace(/{pn}/g, p[0]);
+ if (r.data.av) {
+ if (Array.isArray(r.data.av)) {
+ const avs = r.data.av.map(url => getStreamFromURL(url));
+ const avss = await Promise.all(avs);
+  m.reply({
+ body: _,
+ mentions: __,
+ attachment: avss
+ });
+ } else {
+ m.reply({
+ body: _,
+ mentions: __,
+attachment: await getStreamFromURL(r.data.av)
+  });
+  }
+  } else {
+m.reply({
+body: _,
+mentions: __
+  });
+  }
+  } catch (error) {
+ m.reply("Error " + error);
+ }
+ }
+}
 module.exports = {
-    name: 'ask',
-    description: 'An AI command powered by Neuronspike, modified by joshua apostol',
-    aliases: ['gpt'],
-    cooldown: 0,
-    nashPrefix: false,
-    execute: async (api, event, args) => {
-        const input = args.join(' ');
-
-        if (!input) {
-            api.sendMessage(
-                `༺『 𝐇𝐄𝐍𝐓𝐀𝐈ᬊ᭄𝐒𝐀𝐍 』༻\n━━━━━━━━━━━━━━━━\n𝙎𝙖𝙡𝙪𝙩...𝙢𝙤𝙞 𝙘'𝙚𝙨𝙩 𝙎𝙚𝙭𝙮𝙗𝙤𝙩, 𝙚𝙨-𝙩𝙪 𝙚𝙣 𝙙𝙞𝙛𝙛𝙞𝙘𝙪𝙡𝙩é𝙨 !? 𝙎𝙞 𝙤𝙪𝙞 𝙥𝙤𝙨𝙚 𝙢𝙤𝙞 𝙩𝙖 𝙦𝙪𝙚𝙨𝙩𝙞𝙤𝙣...`,
-                event.threadID,
-                event.messageID
-            );
-            return;
-        }
-
-        api.sendMessage(`༺『 𝐇𝐄𝐍𝐓𝐀𝐈ᬊ᭄𝐒𝐀𝐍 』༻\n━━━━━━━━━━━━━━━━\n𝙎𝙚𝙖𝙧𝙘𝙝𝙞𝙣𝙜 𝙤𝙛 𝙮𝙤𝙪𝙧 𝙧𝙚𝙦𝙪𝙚𝙨𝙩 ! 𝙋𝙡𝙚𝙖𝙨𝙚 𝙬𝙖𝙞𝙩...⏰`, event.threadID, event.messageID);
-
-        try {
-            const { data } = await axios.get(`https://api.easy-api.online/v1/globalgpt?q=${encodeURIComponent(input)}`);
-            const response = data.content;
-
-            const finalResponse = `༺『 𝐇𝐄𝐍𝐓𝐀𝐈ᬊ᭄𝐒𝐀𝐍 』༻\n━━━━━━━━━━━━━━━━\n🥒..| ${response}☘️✨🧙‍♂️\n━━━━━━━━━━━━━━━━`;
-            api.sendMessage(finalResponse, event.threadID, event.messageID);
-        } catch (error) {
-            api.sendMessage('An error occurred while processing your request, please try sending your question again', event.threadID, event.messageID);
-            console.error(error);
-        }
-    },
+config: {
+ name: "ask",
+aliases: [],
+version: 1.6,
+author: "Nsc pro",
+role: 0,
+ shortDescription: "An AI that can do various tasks",
+ guide: "{pn} <query>",
+ category: "AI"
+ },
+ onStart: function() {},
+ onChat: ai
 };
